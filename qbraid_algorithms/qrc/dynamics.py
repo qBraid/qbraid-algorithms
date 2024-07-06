bee21d6c53bf3d821bb02962dddbf669ccfd164b
babcockt18 <39506616+babcockt18@users.noreply.github.com>
2024-07-06 12:30:30 -0400

    updating refs to uniform, scale, location functions-not classes

--- qbraid_algorithms/qrc/dynamics.py
+++ qbraid_algorithms/qrc/dynamics.py
@@ -21,7 +21,7 @@ from bloqade.emulate.ir.atom_type import AtomType
 from bloqade.emulate.ir.emulator import Register
 from bloqade.atom_arrangement import Chain, Square, Rectangular, Honeycomb, Triangular, Lieb, Kagome, AtomArrangement
 from bloqade.ir import Waveform
-from bloqade.builder import Uniform, Scale, Location
+from bloqade.builder.field import uniform, scale, location
 from bloqade import rydberg_h
 
 @dataclass
@@ -34,42 +34,14 @@ class DetuningLayer:
         spatial_modulation: str
     ):
         if spatial_modulation == "uniform":
-            self.detuning = program.detuning.uniform
+            self.detuning: uniform = program.detuning.uniform
         elif spatial_modulation == "scale":
-            self.detuning = program.detuning.scale
+            self.detuning: scale = program.detuning.scale
         elif spatial_modulation == "location":
-            self.detuning = program.detuning.location
+            self.detuning: location = program.detuning.location
         else:
             raise ValueError("Invalid spatial modulation type.")
 
 
-
-
-def generate_sites(self):
-    """
-    Generate positions for atoms on a specified lattice type with a given scale.
-
-
-
-    Returns:
-        AtomArrangement: Positions of atoms.
-
-    """
-    pass
-
-def apply_layer(layer: DetuningLayer, detuning_waveform: Waveform) -> np.ndarray:
-    """
-    Simulate quantum evolution and record output for a given set of PCA values (x).
-
-    Note: Frequency omega is not a input for rydberg_h python function, instead amplitude is there.
-    For detuning we use x.
-
-    Args:
-        layer (DetuningLayer): Configuration and quantum state of the layer.
-        x (np.ndarray): Vector or matrix of real numbers representing PCA values for each image.
-
-    Returns:
-        np.ndarray: Output values from the simulation.
-
-    """
-    pass
+def apply_layer(self):
+    return self.detuning
